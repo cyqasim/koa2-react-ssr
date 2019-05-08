e266818dc2aae6c19c5ca9a459d3f58bdfde7505
chenyiqiu <chenyiqiu@wshifu.com>
2019-05-08 17:23:33 +0800

    test

--- client/components/loading.js
+++ client/components/loading.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 
 export default class Loading extends Component {
-    render () {
-        return (
-            <div className='loading-box'>
-                加载肿
-            </div>
-        );
+    render() {
+        return <div className="loading-box">加载肿</div>;
     }
 }
